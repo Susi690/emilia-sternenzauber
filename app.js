290a8072af77940f29386f2c3897d6609e5b66ed
Susi690 <s.naumann11@web.de>
2026-07-09 16:22:33 +0200

    Erste Version

---
+++ app.js
@@ -0,0 +1,55 @@
+let stars = Number(localStorage.getItem("stars")) || 0;
+
+const startPage = document.getElementById("startPage");
+const taskPage = document.getElementById("taskPage");
+
+const startButton = document.getElementById("startButton");
+const backButton = document.getElementById("backButton");
+
+const starsCountStart = document.getElementById("starsCountStart");
+const starsCountTasks = document.getElementById("starsCountTasks");
+
+const progressBarStart = document.getElementById("progressBarStart");
+const progressBarTasks = document.getElementById("progressBarTasks");
+
+const taskChecks = document.querySelectorAll(".taskCheck");
+
+startButton.addEventListener("click", function () {
+startPage.classList.remove("active");
+taskPage.classList.add("active");
+});
+
+backButton.addEventListener("click", function () {
+taskPage.classList.remove("active");
+startPage.classList.add("active");
+});
+
+taskChecks.forEach((check, index) => {
+const saved = localStorage.getItem("task-" + index);
+
+if (saved === "true") {
+    check.checked = true;
+    check.parentElement.classList.add("done");
+}
+
+
+check.addEventListener("change", function () {
+localStorage.setItem("task-" + index, check.checked);
+check.parentElement.classList.toggle("done", check.checked); 
+stars = document.querySelectorAll(".taskCheck:checked").length;
+localStorage.setItem("stars", stars);
+
+updateStars();
+});
+});
+
+function updateStars() {
+starsCountStart.textContent = stars;
+starsCountTasks.textContent = stars;
+
+const percent = (stars / 35) * 100;
+progressBarStart.style.width = percent + "%";
+progressBarTasks.style.width = percent + "%";
+}
+
+updateStars(); 
